5452c0d5c917834f314db0c0a2708acbdcc2a2ec
michalgorul <michalgorul@gmail.com>
2022-06-06 23:14:37 +0200

    add facenet model

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import cv2 # opencv
+from tensorflow.python.keras.models import load_model
 
 from facenet.facenet import Facenet
 from facenet.utils import show_image, show_extracted_face
 
 if __name__ == '__main__':
-    img = cv2.imread('../input/data/train/ben_afflek/httpcsvkmeuaeccjpg.jpg')
-    show_image(img)
-    show_extracted_face('../input/data/train/ben_afflek/httpcsvkmeuaeccjpg.jpg')
-
+    # img = cv2.imread('../input/data/train/ben_afflek/httpcsvkmeuaeccjpg.jpg')
+    # show_image(img)
+    # show_extracted_face('../input/data/train/ben_afflek/httpcsvkmeuaeccjpg.jpg')
     facenet = Facenet()
+    facenet.setup()
--- facenet/facenet.py
+++ facenet/facenet.py
@@ -1,7 +1,9 @@
 import os
+from typing import Tuple
 
 import numpy as np
 from numpy import ndarray
+from tensorflow.python.keras.models import load_model
 
 from facenet.utils import load_faces_from_dir
 
@@ -10,10 +12,29 @@ class Facenet:
     data_train_path = '../input/data/train/'
     data_val_path = '../input/data/val/'
 
-    def __int__(self):
-        self._load_train_test_datasets()
+    def __int__(self) -> None:
+        self.trainX = None
+        self.trainY = None
+        self.testX = None
+        self.testY = None
+        self.facenet_model = None
 
-    def _load_train_test_datasets(self):
+    def setup(self):
+        self._load_train_test_datasets()
+        self._load_the_facenet_dataset()
+        self._load_facenet_model()
+
+    def _load_the_facenet_dataset(self) -> None:
+        data = np.load('5-celebrity-faces-dataset.npz')
+        self.trainX, self.trainY, self.testX, self.testY = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+        print('Loaded: ', self.trainX.shape, self.trainY.shape, self.testX.shape, self.testY.shape)
+
+    # TODO find proper model for our TF version
+    def _load_facenet_model(self) -> None:
+        self.facenet_model = load_model('facenet_keras.h5')
+        print('Loaded Model')
+
+    def _load_train_test_datasets(self) -> None:
         print('\nload train dataset')
         trainX, trainy = self.load_dataset(self.data_train_path)
         print(trainX.shape, trainy.shape)
@@ -24,7 +45,7 @@ class Facenet:
         # save and compress the dataset for further use
         np.savez_compressed('5-celebrity-faces-dataset.npz', trainX, trainy, testX, testy)
 
-    def load_dataset(self, path_dir: str) -> tuple[ndarray, ndarray]:
+    def load_dataset(self, path_dir: str) -> Tuple[ndarray, ndarray]:
         # list for faces and labels
         X, y = list(), list()
         for subdir in os.listdir(path_dir):
