fe50779527121cdd143e6fa0e0584547b2f23ca2
michalgorul <michalgorul@gmail.com>
2022-06-06 23:33:17 +0200

    add todo

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from tensorflow.python.keras.models import load_model
 from facenet.facenet import Facenet
 from facenet.utils import show_image, show_extracted_face
 
+# TODO TF 2.2, Keras 2.3 or 2.4, Python 3.6.
 if __name__ == '__main__':
     # img = cv2.imread('../input/data/train/ben_afflek/httpcsvkmeuaeccjpg.jpg')
     # show_image(img)
