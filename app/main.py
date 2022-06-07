d32b95cd460f748c61cd851983803e24f30a08dd
michalgorul <michalgorul@gmail.com>
2022-06-07 21:26:36 +0200

    add embedding and calculating accuracy

--- app/main.py
+++ app/main.py
@@ -1,13 +1,6 @@
-import cv2 # opencv
-from tensorflow.python.keras.models import load_model
-
 from facenet.facenet import Facenet
-from facenet.utils import show_image, show_extracted_face
 
-# TODO TF 2.2, Keras 2.3 or 2.4, Python 3.6.
 if __name__ == '__main__':
-    # img = cv2.imread('../input/data/train/ben_afflek/httpcsvkmeuaeccjpg.jpg')
-    # show_image(img)
-    # show_extracted_face('../input/data/train/ben_afflek/httpcsvkmeuaeccjpg.jpg')
     facenet = Facenet()
     facenet.setup()
+    facenet.calculate_accuracy()
--- facenet/facenet.py
+++ facenet/facenet.py
@@ -2,8 +2,11 @@ import os
 from typing import Tuple
 
 import numpy as np
-from numpy import ndarray
 from keras.models import load_model
+from numpy import ndarray
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import Normalizer, LabelEncoder
+from sklearn.svm import SVC
 
 from facenet.utils import load_faces_from_dir
 
@@ -12,6 +15,7 @@ class Facenet:
     data_train_path = '../input/data/train/'
     data_val_path = '../input/data/val/'
     celebrity_faces_dataset_path = '../resources/5-celebrity-faces-dataset.npz'
+    facenet_keras_path = '../resources/facenet_keras.h5'
 
     def __int__(self) -> None:
         self.trainX = None
@@ -19,11 +23,52 @@ class Facenet:
         self.testX = None
         self.testY = None
         self.facenet_model = None
+        self.emdTrainX = None
+        self.emdTestX = None
 
     def setup(self):
         # self._load_train_test_datasets()
-        # self._load_the_facenet_dataset()
+        self._load_the_facenet_dataset()
         self._load_facenet_model()
+        self._convert_train_test_faces_into_embedding()
+
+    def calculate_accuracy(self):
+        print("Dataset: train=%d, test=%d" % (self.emdTrainX.shape[0], self.emdTestX.shape[0]))
+        # normalize input vectors
+        in_encoder = Normalizer()
+        emdTrainX_norm = in_encoder.transform(self.emdTrainX)
+        emdTestX_norm = in_encoder.transform(self.emdTestX)
+        # label encode targets
+        out_encoder = LabelEncoder()
+        out_encoder.fit(self.trainY)
+        trainy_enc = out_encoder.transform(self.trainY)
+        testy_enc = out_encoder.transform(self.testY)
+        # fit model
+        model = SVC(kernel='linear', probability=True)
+        model.fit(emdTrainX_norm, trainy_enc)
+        # predict
+        yhat_train = model.predict(emdTrainX_norm)
+        yhat_test = model.predict(emdTestX_norm)
+        # score
+        score_train = accuracy_score(trainy_enc, yhat_train)
+        score_test = accuracy_score(testy_enc, yhat_test)
+        # summarize
+        print('Accuracy: train=%.3f, test=%.3f' % (score_train * 100, score_test * 100))
+
+    def _convert_train_test_faces_into_embedding(self):
+        self.emdTrainX = self._convert_faces_into_embedding(self.trainX)
+        self.emdTestX = self._convert_faces_into_embedding(self.testX)
+        np.savez_compressed('5-celebrity-faces-embeddings.npz', self.emdTrainX, self.trainY,
+                            self.emdTestX, self.testY)
+
+    def _convert_faces_into_embedding(self, faces) -> ndarray:
+        emd_faces = list()
+        for face in faces:
+            emd = self._get_embedding(self.facenet_model, face)
+            emd_faces.append(emd)
+
+        emd_faces = np.asarray(emd_faces)
+        return emd_faces
 
     def _get_embedding(self, model, face):
         # scale pixel values
@@ -37,29 +82,31 @@ class Facenet:
         yhat = model.predict(sample)
         return yhat[0]
 
-    def _load_the_facenet_dataset(self) -> None:
-        data = np.load(self.celebrity_faces_dataset_path)
-        self.trainX, self.trainY, self.testX, self.testY = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
-        print('Loaded: ', self.trainX.shape, self.trainY.shape, self.testX.shape, self.testY.shape)
-
     def _load_facenet_model(self) -> None:
         # load the model
-        self.facenet_model = load_model('facenet_keras.h5')
+        self.facenet_model = load_model(self.facenet_keras_path)
         # summarize input and output shape
         print('Model Loaded')
         print(self.facenet_model.inputs)
         print(self.facenet_model.outputs)
 
+    def _load_the_facenet_dataset(self) -> None:
+        data = np.load(self.celebrity_faces_dataset_path)
+        self.trainX, self.trainY, self.testX, self.testY = data['arr_0'], data['arr_1'], data[
+            'arr_2'], data['arr_3']
+        print('Loaded: trainX.shape: ', self.trainX.shape, ' trainY.shape: ', self.trainY.shape,
+              ' testX.shape: ', self.testX.shape, ' testY.shape: ', self.testY.shape)
+
     def _load_train_test_datasets(self) -> None:
         print('\nload train dataset')
-        trainX, trainy = self.load_dataset(self.data_train_path)
-        print(trainX.shape, trainy.shape)
+        trainX, trainY = self.load_dataset(self.data_train_path)
+        print('trainX.shape: ', trainX.shape, ' trainY.shape: ', trainY.shape)
         print('\nload test dataset')
-        testX, testy = self.load_dataset(self.data_val_path)
-        print(testX.shape, testy.shape)
+        testX, testY = self.load_dataset(self.data_val_path)
+        print('testX.shape: ', testX.shape, ' testY.shape: ', testY.shape)
 
         # save and compress the dataset for further use
-        np.savez_compressed(self.celebrity_faces_dataset_path, trainX, trainy, testX, testy)
+        np.savez_compressed(self.celebrity_faces_dataset_path, trainX, trainY, testX, testY)
 
     def load_dataset(self, path_dir: str) -> Tuple[ndarray, ndarray]:
         # list for faces and labels
